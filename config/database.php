acba6bbfb774f767afe64b6ee4bc813178a908b8
enes1004 <karabacakenes@gmail.com>
2023-04-08 11:21:31 +0900

    add local mysql settings and oauth client dump

--- config/database.php
+++ config/database.php
@@ -60,6 +60,24 @@ return [
             'engine' => null,
         ],
 
+        'mysql_local' => [
+            'driver' => 'mysql',
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'strict' => true,
+            'engine' => null,
+        ],
+
+
+
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
--- install.sh
+++ install.sh
@@ -2,3 +2,14 @@ cat .env.example > .env
 php artisan key:generate
 php artisan migrate
 php artisan db:seed
+chmod -R 0777 storage
+php artisan passport:install
+php artisan passport:client --client
+php artisan passport:client
+echo "note secrets!!!!!!!!"
+echo "note secrets!!!!!!!!"
+echo "note secrets!!!!!!!!"
+echo "note secrets!!!!!!!!"
+echo "note secrets!!!!!!!!"
+echo "note secrets!!!!!!!!"
+echo "note secrets!!!!!!!!"
